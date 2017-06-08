8154af1a2578cff9ae135ea64156f2f18ca6d7b7
cyuanyang <shawn199101@163.com>
2017-06-08 22:05:25 +0800

    11

--- app/src/main/java/com/cyy/widgetkitsimple/simple/soundsimple/SoundSimpleActivity.java
+++ app/src/main/java/com/cyy/widgetkitsimple/simple/soundsimple/SoundSimpleActivity.java
@@ -1,17 +1,25 @@
 package com.cyy.widgetkitsimple.simple.soundsimple;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
+import com.cyy.sound.Sound.SpeakButton;
 import com.cyy.widgetkitsimple.R;
 
 public class SoundSimpleActivity extends AppCompatActivity {
 
+    protected SpeakButton speakBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sound_simple);
+        super.setContentView(R.layout.activity_sound_simple);
+        initView();
 
 
+    }
+
+    private void initView() {
+        speakBtn = (SpeakButton) findViewById(R.id.speakBtn);
     }
 }
