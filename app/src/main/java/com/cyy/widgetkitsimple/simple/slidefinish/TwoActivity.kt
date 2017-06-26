76a2e728b22d91eda035d3fe3e57042ea6fd2e73
yuanyang <yuanyang.chen@xstudy.com.cn>
2017-06-26 14:32:20 +0800

    finish layout

--- app/src/main/java/com/cyy/widgetkitsimple/simple/slidefinish/TwoActivity.kt
+++ app/src/main/java/com/cyy/widgetkitsimple/simple/slidefinish/TwoActivity.kt
@@ -3,24 +3,17 @@ package com.cyy.widgetkitsimple.simple.slidefinish
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import com.cyy.widgetkitsimple.R
 import kotlinx.android.synthetic.main.activity_slide_finish_simple.*
 
-
-/**
- * Slide finish Simple
- */
-class SlideFinishSimple : AppCompatActivity() {
+class TwoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_slide_finish_simple)
+        setContentView(R.layout.activity_two)
 
         nextBtn.setOnClickListener {
-            Log.e("TAg" , "setOnClickListener")
             startActivity(Intent(this , TwoActivity::class.java))
         }
-
     }
 }
--- slidefinishlayout/src/main/java/com/cyy/slidefinish/SlideFinishLayout.kt
+++ slidefinishlayout/src/main/java/com/cyy/slidefinish/SlideFinishLayout.kt
@@ -3,7 +3,10 @@ package com.cyy.slidefinish
 import android.content.Context
 import android.util.AttributeSet
 import android.util.Log
+import android.view.MotionEvent
+import android.view.ViewConfiguration
 import android.widget.FrameLayout
+import android.widget.Scroller
 
 /**
  * Created by study on 17/6/26.
@@ -12,22 +15,88 @@ import android.widget.FrameLayout
  */
 class SlideFinishLayout:FrameLayout{
 
-    var TAG:String = "SlideFinishLayout"
+    val TAG:String = "SlideFinishLayout"
+    val MIN:Int = 100 //px 最小滑动的距离 超出这个认为是滑动返回
+
+    var mScroller:Scroller
+    var mIsDrag:Boolean = false
+    var mLastX:Int = 0
+    var mTouchSlop:Int = 0
+
+    var mCurrentOffset:Int = 0
+
+    init {
+        Log.e(TAG , "init")
+        mScroller = Scroller(context)
+    }
 
     constructor(context: Context?) : super(context){
-        init()
+        initMyView()
     }
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs){
-        init()
+        initMyView()
     }
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr){
-        init()
+        initMyView()
     }
 
 
-    fun init(){
-        Log.e(TAG , "init")
+    fun initMyView(){
+        Log.e(TAG , "initMyView")
+        var viewConfig:ViewConfiguration = ViewConfiguration.get(context)
+        mTouchSlop = viewConfig.scaledTouchSlop
+    }
 
+    override fun onInterceptTouchEvent(ev: MotionEvent): Boolean {
+        var action = ev.action
+        when (action){
+            MotionEvent.ACTION_DOWN -> {
+                mLastX = ev.x.toInt()
+                mIsDrag = false
+            }
+            MotionEvent.ACTION_MOVE -> {
+                mScroller.computeScrollOffset()
+                mIsDrag = !mScroller.isFinished
+
+                var deltaY:Int = ev.x.toInt() - mLastX
+                if (deltaY >= mTouchSlop){
+                    mIsDrag = true
+                }
+            }
+        }
+        return mIsDrag
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+
+        var action = event.action
+
+        when(action){
+            MotionEvent.ACTION_DOWN -> {
+                mLastX = event.x.toInt()
+            }
+            MotionEvent.ACTION_MOVE -> {
+                var currentX = event.x.toInt()
+                var deltaX = currentX - mLastX
+                performDrag(deltaX)
+                mLastX = currentX
+            }
+            MotionEvent.ACTION_UP,
+            MotionEvent.ACTION_CANCEL ->{
+                mLastX = 0
+                mIsDrag = false
+                mCurrentOffset = 0
+            }
+        }
+        return true
+    }
+
+    //开始拖动
+    fun performDrag(x:Int){
+        Log.e(TAG , "x == $x offset = $mCurrentOffset")
+        mCurrentOffset += x
+        offsetLeftAndRight(mCurrentOffset)
+        Log.e(TAG , "mCurrentOffset = $mCurrentOffset")
     }
 }
 
